8582d59075da0e53aaed4c09859bb953b52a5d09
Nirmit <nirmitsabharwal15@gmail.com>
2020-08-18 23:58:39 +0530

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCtYPJDVWahOQI4t_Bc668xR10uTbqn3W8",
+  authDomain: "whatsapp-clone-15.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-15.firebaseio.com",
+  projectId: "whatsapp-clone-15",
+  storageBucket: "whatsapp-clone-15.appspot.com",
+  messagingSenderId: "217273108613",
+  appId: "1:217273108613:web:1de3a4bef849f2c2a081ec",
+  measurementId: "G-FMB9KKV5TP"
+};
